6d1c2b66b9c433456fd52c9234d7bf2da4fe9d72
Michael Penrod <mkpenrod05@gmail.com>
2013-07-31 23:34:13 -0600

    Made changes following a Youtube video.

---
+++ WODpile/WODpile/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//adding this namespace allows access to any model classes declared in the Models folder. 
+using WODpile.Models;
+
+namespace WODpile.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Details()
+        {
+            User CurrentUser = new User()
+            {
+                ID = 12,
+                UserName = "Joe Schmoe"
+            };
+            
+            return View();
+        }
+
+    }
+}
---
+++ WODpile/WODpile/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WODpile.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+    }
+
+    [Table("***Table Name***")]
+    public class UserData : DbContext 
+    {
+        public DbSet<User> UserData { get; set; }    
+    }
+}
